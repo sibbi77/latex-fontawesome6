ee11167b9f393640dbd5211030f40b68f2b8ccd8
Sebastian Held <sebastian.held@gmx.de>
2024-01-07 12:07:44 +0100

    add sharp style (pro version)

--- create_sty.py
+++ create_sty.py
@@ -23,7 +23,19 @@ OUTPUT_HEADER = r'''
     UprightFont=*-Regular-400,
     ItalicFont=*-Light-300,
     BoldFont=*-Solid-900,
+    BoldItalicFont=*-Thin-100,
   ]{Font Awesome 6 Pro}
+  \newfontfamily\FAduotone[
+    Path=\@fontdir,
+    UprightFont=*-Solid-900,
+  ]{Font Awesome 6 Duotone}
+  \newfontfamily\FAsharp[
+    Path=\@fontdir,
+    UprightFont=*-Regular-400,
+    ItalicFont=*-Light-300,
+    BoldFont=*-Solid-900,
+    BoldItalicFont=*-Thin-100,
+  ]{Font Awesome 6 Sharp}
 }
 \ProcessOptions\relax
 % Define shortcut to load the Font Awesome font for brands.
@@ -37,12 +49,23 @@ OUTPUT_HEADER = r'''
 ]{Font Awesome 6 Free}
 }{}
 % Generic command displaying an icon by its name.
-\newcommand*{\faicon}[1]{{
-  \csname faicon@#1\endcsname
+\def \ifempty#1{\def\temp{#1} \ifx\temp\empty }
+\newcommand*{\faicon}[2][]{{
+  \ifempty{#1}\csname faicon@#2\endcsname\else\csname faicon@#1@#2\endcsname\fi
 }}
 '''
 
-OUTPUT_LINE = '\expandafter\def\csname faicon@%(name)s\endcsname {%(font)s\symbol{"%(symbol)s}} \n'
+OUTPUT_LINE = '\expandafter\def\csname faicon@%(type)s%(name)s\endcsname {%(font)s\symbol{"%(symbol)s}} \n'
+
+def write_line(w, icon_name, icons, font, type):
+    w.write(
+        OUTPUT_LINE % {
+            'name': icon_name,
+            'symbol': icons[icon_name]["unicode"].upper(),
+            'font': font,
+            'type': type
+        }
+    )
 
 def main():
     with open(INPUT_FILE, 'r') as json_data:
@@ -50,12 +73,17 @@ def main():
         with open(OUTPUT_FILE, 'w') as w:
             w.write(OUTPUT_HEADER)
             for icon_name in sorted(icons.keys()):
-                font = "\FA" if "brands" not in icons[icon_name]["styles"] else "\FAbrands"
-                w.write(
-                    OUTPUT_LINE % {
-                        'name': icon_name, 'symbol': icons[icon_name]["unicode"].upper(), "font": font
-                    }
-                )
+                if "brands" in icons[icon_name]["styles"]:
+                    font = "\FAbrands"
+                    type = ""
+                    write_line(w, icon_name, icons, font, type)
+                if "duotone" in icons[icon_name]["styles"]:
+                    font = "\FAduotone"
+                    type = "duotone@"
+                    write_line(w, icon_name, icons, font, type)
+                if "regular" in icons[icon_name]["styles"]:
+                    write_line(w, icon_name, icons, "\FA", "")
+                    write_line(w, icon_name, icons, "\FAsharp", "sharp@")
             w.write(r'\endinput')
 
 
